1a7ec0b65603fdd97bccf8a328b46f1183607bc7
Eric-1dev <eric-1@yandex.ru>
2020-09-23 21:10:12 +0400

    Adding gitignore stage_2

---
+++ Album/Album.DAL.MSSQL/SQLAlbumDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Album.DAL.MSSQL
+{
+    public class SQLAlbumDAL
+    {
+
+    }
+}
